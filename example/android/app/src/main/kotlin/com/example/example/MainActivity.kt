fd54321dbece57bb16af277ca4754247f8922704
josxha <34318751+josxha@users.noreply.github.com>
2023-05-10 23:37:14 +0200

    add native integrations for example project
    flutter create .

---
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.ibrierley.flutter_map_line_editor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/android/app/src/main/kotlin/com/github/ibrierley/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.ibrierley.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
